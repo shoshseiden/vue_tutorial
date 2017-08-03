c19b63486d05168722361cd769be84a55bdcea37
Shosh Seiden <shosh.j.seiden@gmail.com>
2017-08-02 21:20:54 -0600

    List Products

--- exercise-files/list-products/src/App.vue
+++ exercise-files/list-products/src/App.vue
@@ -3,14 +3,20 @@
   lowercased and dasherized. e.g. ProductList -> product-list -->
 
   <!-- use the <product-list> component here! -->
+  <product-list></product-list>
 </template>
 
 <script>
 // import product list component
+import ProductList from './components/ProductList';
 
 export default {
   components: {
+    // With ES6 we can use the Object Literal Shorthand Syntax to save us some
+    // keystrokes
     // add product list as a component
+    ProductList
+
   }
 }
 </script>
